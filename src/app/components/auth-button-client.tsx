9cc8b4d147f91135ca45117868b9f039307884a0
David Requeno <davidrequeno52@gmail.com>
2023-08-25 20:57:44 -0600

    mostrando botones

---
+++ src/app/components/auth-button-client.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { type Session, createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { GithubIcon } from './icons'
+import { useRouter } from 'next/navigation'
+
+export function AuthButton ({ session }: { session: Session | null }) {
+  const supabase = createClientComponentClient()
+  const router = useRouter()
+
+  const handleSignIn = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: 'github',
+      options: {
+        redirectTo: 'http://localhost:3000/auth/callback'
+      }
+    })
+  }
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut()
+    router.refresh()
+  }
+
+  return (
+    <header>
+      {
+        session === null
+          ? (
+          <button onClick={handleSignIn} type="button" className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2">
+  <GithubIcon/>
+  Sign in with Github
+</button>
+            )
+          : (
+          <button onClick={handleSignOut} type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Sign Out</button>
+            )
+      }
+
+    </header>
+  )
+}
