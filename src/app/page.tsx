1d601509cb0faee846f9944eee065518c3153232
David Requeno <davidrequeno52@gmail.com>
2023-09-01 21:25:15 -0600

    estilos de formulario para crear posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import AuthButtonServer from './components/auth-button-server'
 import { redirect } from 'next/navigation'
 import PostList from './components/post-list'
 import { type Database } from './types/database'
+import ComposePost from './components/compose-post'
 
 export default async function Home () {
   const supabase = createServerComponentClient<Database>({ cookies })
@@ -20,10 +21,11 @@ export default async function Home () {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <section className='min-w-[800px] mx-auto border-l border-r border-white/20 min-h-screen'>
-        <AuthButtonServer/>
+      <section className='max-w-[600px] w-full mx-auto border-l border-r border-white/20 min-h-screen'>
+        <ComposePost userAvatarUrl={session.user?.user_metadata?.avatar_url}/>
         <PostList posts={posts}/>
       </section>
+        <AuthButtonServer/>
     </main>
   )
 }
